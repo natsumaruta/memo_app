5af78e9609e6894d61c9997f293ef36319407464
yonezawanatsuko <nabeta0915@gmail.com>
2025-12-06 18:29:54 +0900

    プロジェクトの準備完了

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -16,10 +16,10 @@ class DatabaseSeeder extends Seeder
     public function run(): void
     {
         // User::factory(10)->create();
-
-        User::factory()->create([
-            'name' => 'Test User',
-            'email' => 'test@example.com',
-        ]);
+        //開発環境（local（本番環境だったらprodaほにゃらら））だったら、
+        if (config('app.env')=='local') {
+            $this->call(MemoSeeder::class);
+        }
+        
     }
 }
